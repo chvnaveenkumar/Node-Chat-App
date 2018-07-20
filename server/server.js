10fc750e87ea00a030d7a0a60b70eed40f75e0d9
Naveen Kumar Chandaluri <S530742@nwmissouri.edu>
2018-07-19 20:49:01 -0500

    Multiple Chat application

--- server/server.js
+++ server/server.js
@@ -1,12 +1,41 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname+'/../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+    console.log('New user connected');
+
+    socket.emit('newMessage',{
+        from: 'Server',
+        text:'Message from the server',
+        createAt: 123123
+    });
+
+    socket.on('createMessage',(message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () =>{
+        console.log('User was disconnected');
+    });
+});
+
+
+
+server.listen(port, () => {
     console.log(`server is upon port ${port}`);
 });
